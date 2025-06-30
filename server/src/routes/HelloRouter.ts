9eee7fa7baa7d705b22c094f6376903e3ca5c18f
migayalach <ayalachavezmiguel@gmail.com>
2025-06-29 22:58:53 -0400

    route hello

---
+++ server/src/routes/HelloRouter.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import { HelloController } from "../controllers/HelloController";
+import { LogInfo } from "../utils/logger";
+
+// Router from express
+const helloRouter = express.Router();
+
+// http://localhost:3001/api/hello?name=Martin
+helloRouter
+  .route("/")
+  //GET => http://localhost:3001/api/hello?name=Martin
+  .get(async (request: Request, response: Response) => {
+    // Obtain a Query Param
+    const name: any = request?.query?.name;
+    LogInfo(`Query param: ${name}`);
+    // Controller Instance to excute method
+    const controller: HelloController = new HelloController();
+    // Obtain Response
+    const results = await controller.getMessage(name);
+    // Send to the client the response
+    response.send(results);
+  });
+
+// Export Hello Router
+export default helloRouter;
