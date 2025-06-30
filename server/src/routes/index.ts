54a6715ab0a39c9aebd24b8e0b93fcfac6da8960
migayalach <ayalachavezmiguel@gmail.com>
2025-06-29 22:59:36 -0400

    server created with success

---
+++ server/src/routes/index.ts
@@ -0,0 +1,26 @@
+/**
+ * Root Router
+ * Redirections to Routers
+ */
+
+import express, { Response, Request } from "express";
+import HelloRouter from "./HelloRouter";
+import { LogInfo } from "../utils/logger";
+
+// Server instance
+const server = express();
+
+// Router instance
+const rootRouter = express.Router();
+
+// Acticate for requests to http:localhost:3001/api
+rootRouter.get("/", (request: Request, response: Response) => {
+  LogInfo("GET: http://localhost:3001/api");
+  response.send("Welcome to my API");
+});
+
+// Redirections to Routers & Controllers
+server.use("/", rootRouter); //http://localhost:3001/api/
+server.use("/hello", HelloRouter); //http://localhost:3001/api/hello --> Hello Router
+// Add more routes to the app
+export default server;
