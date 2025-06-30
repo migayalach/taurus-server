28efc2d4b5c01af01d219ddb74a2073a0ec31e8c
migayalach <ayalachavezmiguel@gmail.com>
2025-06-30 00:24:27 -0400

    comment controller

--- server/src/controllers/HelloController.ts
+++ server/src/controllers/HelloController.ts
@@ -1,9 +1,18 @@
+import { Get, Query, Route, Tags } from "tsoa";
 import { BasicResponse } from "../types";
 import { IHelloController } from "../interfaces";
 import { LogSuccess } from "../utils/logger";
 
+@Route("/api/hello")
+@Tags("HelloController")
 export class HelloController implements IHelloController {
-  public async getMessage(name?: string): Promise<BasicResponse> {
+  /**
+   * Enpoint to retreive a Message 'Hello, {name}' in JSON
+   * @param {string | undefined} name Name of user to be greeted
+   * @returns {BasicResponse} Promise of Basic Response
+   */
+  @Get("/")
+  public async getMessage(@Query() name?: string): Promise<BasicResponse> {
     LogSuccess("[/api] Get Request");
     return {
       message: `Hello, ${name || "world"}.`,
