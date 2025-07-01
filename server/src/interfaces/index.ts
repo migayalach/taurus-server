cc6dd0a83a46defcd42a8a90273e718c483fa906
migayalach <ayalachavezmiguel@gmail.com>
2025-06-30 23:52:43 -0400

    added new interface to getUsers

--- server/src/interfaces/index.ts
+++ server/src/interfaces/index.ts
@@ -3,3 +3,8 @@ import { BasicResponse } from "../types";
 export interface IHelloController {
   getMessage(name?: string): Promise<BasicResponse>;
 }
+
+export interface IUserController {
+  // Real all users from database || Get User By ID
+  getUsers(id?: string): Promise<any>;
+}
