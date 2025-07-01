fa007de365d227abfc3189848a6c5b2d54481d86
migayalach <ayalachavezmiguel@gmail.com>
2025-06-30 23:52:15 -0400

    created getUserByID

--- server/src/domain/orm/user.orm.ts
+++ server/src/domain/orm/user.orm.ts
@@ -1,23 +1,32 @@
 import { Delete } from "tsoa";
 import { userEntity } from "../entities/user.entity";
-import { LogError, LogSuccess } from "@/utils/logger";
+import { LogError, LogSuccess } from "../../utils/logger";
 
 // CRUD PETICIONS
 /**
  * Nethod to obtain all Users from Collection 'Users' in Mongo Server
  */
-export const GetAllUsers = async (): Promise<any[] | undefined> => {
+export const getAllUsers = async (): Promise<any[] | undefined> => {
   try {
     const userModel = userEntity();
-    //Search all users
-    return await userModel.find({ isDelete: false });
+    return await userModel.find();
   } catch (error) {
     LogError(`[ORM ERROR]: Getting All Users: ${error}`);
   }
 };
 
-// TODO
 // - Get User by ID
+export const getUserByID = async (id: string): Promise<any | undefined> => {
+  try {
+    const userModel = userEntity();
+    // Search User By ID
+    return await userModel.findById(id);
+  } catch (error) {
+    LogError(`[ORM ERROR]: Getting User By ID: ${error}`);
+  }
+};
+
+// TODO
 // - Get User by Email
 // - Delete User by ID
 // - Create new User
