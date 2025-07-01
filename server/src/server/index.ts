04b264019a9969bbf6af752bf9650fdb1f408a75
migayalach <ayalachavezmiguel@gmail.com>
2025-06-30 23:58:00 -0400

    created mongoose conection

--- server/src/server/index.ts
+++ server/src/server/index.ts
@@ -1,15 +1,31 @@
 import express, { Express, Request, Response } from "express";
+// Swagger
+import swaggerUi from "swagger-ui-express";
 
 // Security
 import cors from "cors";
 import helmet from "helmet";
+
 //TODO HTTPS
 
 // Root Router
 import rootRouter from "../routes";
+import mongoose from "mongoose";
 
 // *Create Express APP
 const server: Express = express();
+
+// *Swagger Config and route
+server.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+      exporer: true,
+    },
+  })
+);
 
 // *Define SERVER to use '/api' and use rootRouter from 'index.ts' in routes
 // From this point onover: http://localhost:3001/api/...
@@ -19,6 +35,7 @@ server.use("/api", rootRouter);
 server.use(express.static("public"));
 
 // TODO Mongoose Connection
+mongoose.connect("mongodb://localhost:27017/codeverification");
 
 // *Security Config
 server.use(helmet());
