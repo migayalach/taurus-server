7efabeb0459c32593e78a5b6dae814b55895d43a
migayalach <ayalachavezmiguel@gmail.com>
2025-06-30 23:53:20 -0400

    added method and added type

--- server/src/routes/HelloRouter.ts
+++ server/src/routes/HelloRouter.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { HelloController } from "../controllers/HelloController";
 import { LogInfo } from "../utils/logger";
+import { BasicResponse } from "@/types";
 
 // Router from express
 const helloRouter = express.Router();
@@ -16,7 +17,7 @@ helloRouter
     // Controller Instance to excute method
     const controller: HelloController = new HelloController();
     // Obtain Response
-    const results = await controller.getMessage(name);
+    const results: BasicResponse = await controller.getMessage(name);
     // Send to the client the response
     response.send(results);
   });
