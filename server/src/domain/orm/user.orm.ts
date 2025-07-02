dfdbf54d8aba5d2ebd064d9b626e07602047b976
migayalach <ayalachavezmiguel@gmail.com>
2025-07-02 11:23:30 -0400

    added two methods create and delete

--- server/src/domain/orm/user.orm.ts
+++ server/src/domain/orm/user.orm.ts
@@ -26,8 +26,40 @@ export const getUserByID = async (id: string): Promise<any | undefined> => {
   }
 };
 
+// - Delete User by ID
+export const deleteUserByID = async (id: string): Promise<any | undefined> => {
+  try {
+    const userModel = userEntity();
+    // Delete User By ID
+    return await userModel.deleteOne({ _id: id });
+  } catch (error) {
+    LogError(`[ORM ERROR]: Deleting User By ID: ${error}`);
+  }
+};
+
+// - Create new User
+export const createUser = async (user: any): Promise<any | undefined> => {
+  try {
+    const userModel = userEntity();
+    // Create / Insert new User
+    return await userModel.create(user);
+  } catch (error) {
+    LogError(`[ORM ERROR]: Creating User: ${error}`);
+  }
+};
+
+// - Update User by ID
+export const updateUserByID = async (
+  id: string,
+  user: any
+): Promise<any | undefined> => {
+  try {
+    const userModel = userEntity();
+    return await userModel.findByIdAndUpdate(id, user);
+  } catch (error) {
+    LogError(`[ORM ERROR]: Updating User: ${id}: ${error}`);
+  }
+};
+
 // TODO
 // - Get User by Email
-// - Delete User by ID
-// - Create new User
-// - Update User by ID
