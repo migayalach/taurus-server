fdebab9d5393f5680413f7242e5fce9f4a1e2cf1
migayalach <ayalachavezmiguel@gmail.com>
2025-07-02 11:24:34 -0400

    added thee new interfaces create, delete and update

--- server/src/routes/UsersRouter.ts
+++ server/src/routes/UsersRouter.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from "express";
-import { HelloController } from "../controllers/HelloController";
 import { LogInfo } from "../utils/logger";
 import { UserController } from "../controllers/UsersController";
 
@@ -21,10 +20,55 @@ userRouter
     // Send to the client the response
     response.send(results);
   })
-  .delete(async (request: Request, response: Response) => {});
+  // DELETE:
+  .delete(async (request: Request, response: Response) => {
+    // Obtain a Query Param (ID)
+    const id: any = request?.query?.id;
+    LogInfo(`Query Param: ${id}`);
+    // Controller Instance to excute method
+    const controller: UserController = new UserController();
+    // Obtain Response
+    const results: any = await controller.deleteUser(id);
+    // Send to the client the response
+    response.send(results);
+  })
+  // POST
+  .post(async (request: Request, response: Response) => {
+    // Controller Instance to excute method
+    const controller: UserController = new UserController();
+    const name: any = request?.query?.name;
+    const email: any = request?.query?.email;
+    const age: any = request?.query?.age;
+
+    const user = {
+      name: name || "default",
+      email: email || "default email",
+      age: age || 18,
+    };
+    // Obtain Response
+    const results: any = await controller.createUser(user);
+    // Send to the client the response
+    response.send(results);
+  })
+  .put(async (request: Request, response: Response) => {
+    // Obtain a Query Param (ID)
+    const id: any = request?.query?.id;
+    const name: any = request?.query?.name;
+    const email: any = request?.query?.email;
+    const age: any = request?.query?.age;
+    LogInfo(`Query Param: ${id}, ${name}, ${email}, ${age}`);
+    // Controller Instance to excute method
+    const controller: UserController = new UserController();
+    const user = {
+      name: name || "default",
+      email: email || "default email",
+      age: age || 18,
+    };
+    // Obtain Response
+    const results: any = await controller.updateUser(id, user);
+    // Send to the client the response
+    response.send(results);
+  });
 
 // Export User Router
 export default userRouter;
-
-
-// 46:15
