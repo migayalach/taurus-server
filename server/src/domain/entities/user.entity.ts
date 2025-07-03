05688bf7b7be0d9823597163a35f5987300de687
migayalach <ayalachavezmiguel@gmail.com>
2025-07-03 14:37:16 -0400

    alter entity

--- server/src/domain/entities/user.entity.ts
+++ server/src/domain/entities/user.entity.ts
@@ -1,10 +1,12 @@
+import { IUser } from "../../interfaces/IUser.interface";
 import mongoose from "mongoose";
 
 export const userEntity = () => {
-  const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    age: Number,
+  const userSchema = new mongoose.Schema<IUser>({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    age: { type: Number, required: true },
   });
-  return mongoose.models.Users || mongoose.model("Users", userSchema);
+  return mongoose.models.Users || mongoose.model<IUser>("Users", userSchema);
 };
